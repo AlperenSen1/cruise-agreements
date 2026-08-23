3cfd87f9983ba48c35d2d12d290cd1b5de26036a
Alperen Sen <alperensen039@gmail.com>
2026-08-23 17:56:50 +0300

    Add Handlebars for cruise agreement generation
    Install handlebars and implement the cruise agreement template using
    Handlebars to dynamically render agreement content.

--- src/agreements/templates/cruiseAgreement.ts
+++ src/agreements/templates/cruiseAgreement.ts
@@ -1,4 +1,14 @@
+import Handlebars from "handlebars";
+import templateSource from "./cruiseAgreement.hbs?raw";
 import type { AgreementTemplate } from "../types";
+
+type CruiseAgreementProps = {
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+};
+
+const template = Handlebars.compile<CruiseAgreementProps>(templateSource);
 
 export class CruiseAgreement implements AgreementTemplate {
   templateId = "cruise-agreement";
@@ -6,17 +16,17 @@ export class CruiseAgreement implements AgreementTemplate {
   lastName: string;
   phoneNumber: string;
 
-  constructor(props: {
-    firstName: string;
-    lastName: string;
-    phoneNumber: string;
-  }) {
+  constructor(props: CruiseAgreementProps) {
     this.firstName = props.firstName;
     this.lastName = props.lastName;
     this.phoneNumber = props.phoneNumber;
   }
 
   get = () => {
-    return "";
+    return template({
+      firstName: this.firstName,
+      lastName: this.lastName,
+      phoneNumber: this.phoneNumber,
+    });
   };
 }
